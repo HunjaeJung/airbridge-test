a3ada41e650e7ea42e1913758c4fa0609ae0bb7b
Hunjae Jung <hunjaege@gmail.com>
2017-09-13 11:30:36 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='airbridge-test',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/HunjaeJung/airbridge-test',
+      author='Hunjae',
+      author_email='hunjae@ab180.co',
+      license='MIT',
+      packages=['airbridge'],
+      zip_safe=False)
